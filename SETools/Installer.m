f1c345d1730e725a80af88afb789301fafb14112
halirutan <pscheibe@trm.uni-leipzig.de>
2015-03-23 15:13:20 +0100

    Fixed directory name

--- SETools/Installer.m
+++ SETools/Installer.m
@@ -4,33 +4,34 @@
 
 If[$VersionNumber < 9,
   Print[ "This works only in Mathematica version 9" ],
+  With[{repoName = "Mathematica-SE-Tools-master"}
+    Block[{url,
+      zipFile = FileNameJoin[{$TemporaryDirectory, "master.zip" }],
+      extractedDir = FileNameJoin[{$TemporaryDirectory, repoName }],
+      file
+    },
 
-  Block[{url,
-    zipFile = FileNameJoin[{$TemporaryDirectory, "master.zip" }],
-    extractedDir = FileNameJoin[{$TemporaryDirectory, "SETools-master" }],
-    file
-  },
+      url = "https://github.com/halirutan/Mathematica-SE-Tools/archive/master.zip";
+      If[FileExistsQ[zipFile] || DirectoryQ[extractedDir] || FileNames["SETools", $Path] =!= {} || FileNames["SEUploader", $Path] =!= {},
+        Print[ "Error, the following file/directory already exists. Please remove it and restart:" ];
+        If[FileExistsQ[zipFile], Print[zipFile]];
+        If[DirectoryQ[extractedDir], Print[extractedDir]];
+        If[FileNames["SETools", $Path] =!= {}, Print[FileNames["SETools", $Path]]];
+        If[FileNames["SEUploader", $Path] =!= {}, Print[FileNames["SEUploader", $Path]]];
+        Abort[];
+      ];
 
-    url = "https://github.com/halirutan/Mathematica-SE-Tools/archive/master.zip";
-    If[FileExistsQ[zipFile] || DirectoryQ[extractedDir] || FileNames["SETools", $Path] =!= {} || FileNames["SEUploader", $Path] =!= {},
-      Print[ "Error, the following file/directory already exists. Please remove it and restart:" ];
-      If[FileExistsQ[zipFile], Print[zipFile]];
-      If[DirectoryQ[extractedDir], Print[extractedDir]];
-      If[FileNames["SETools", $Path] =!= {}, Print[FileNames["SETools", $Path]]];
-      If[FileNames["SEUploader", $Path] =!= {}, Print[FileNames["SEUploader", $Path]]];
-      Abort[];
-    ];
-
-    file = URLSave[url, zipFile];
-    If[Not[FileExistsQ[file]],
-      Print[ "Couldn't download resource" ];
-      Abort[];
-    ];
-    ExtractArchive[file, $TemporaryDirectory];
-    CopyDirectory[FileNameJoin[{$TemporaryDirectory, "SETools-master" , "SETools" }],
-      FileNameJoin[{$UserAddOnsDirectory, "Applications" , "SETools" }]];
-    DeleteDirectory[FileNameJoin[{$TemporaryDirectory, "SETools-master" }], DeleteContents -> True];
-    DeleteFile[file];
-    Print[ "Please restart Mathematica to see the palette in the Palette menu" ]
+      file = URLSave[url, zipFile];
+      If[Not[FileExistsQ[file]],
+        Print[ "Couldn't download resource" ];
+        Abort[];
+      ];
+      ExtractArchive[file, $TemporaryDirectory];
+      CopyDirectory[FileNameJoin[{$TemporaryDirectory, repoName , "SETools" }],
+        FileNameJoin[{$UserAddOnsDirectory, "Applications" , "SETools" }]];
+      DeleteDirectory[FileNameJoin[{$TemporaryDirectory, repoName }], DeleteContents -> True];
+      DeleteFile[file];
+      Print[ "Please restart Mathematica to see the palette in the Palette menu" ]
+    ]
   ]
 ]
