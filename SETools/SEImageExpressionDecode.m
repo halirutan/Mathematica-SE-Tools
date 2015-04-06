e89568bcc500e0a3f9faae11108cc971af93f773
halirutan <pscheibe@trm.uni-leipzig.de>
2015-04-06 15:46:15 +0200

    Pointed out how to ignore wrong hashes

--- SETools/SEImageExpressionDecode.m
+++ SETools/SEImageExpressionDecode.m
@@ -24,7 +24,7 @@ SEDecodeImageAndPrint[expr: HoldComplete[_Notebook]] := NotebookPut@@expr;
 SEDecodeImageAndPrint[expr: HoldComplete[__]] := expr;
 
 SEDecodeImage::imp = "Import of `` as png image failed.";
-SEDecodeImage::hash = "The security hash indicates that the data is corrupted.";
+SEDecodeImage::hash = "The security hash indicates that the data is corrupted. Use AbortProtect[SEDecodeImage[...]] to ignore the warning and to proceed at your own risk.";
 SEDecodeImage[url_String] := Module[{img},
   Check[
     img = Import[url, "PNG"],
