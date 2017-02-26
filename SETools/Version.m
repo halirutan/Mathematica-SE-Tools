79bd132929da2c30394c8d6c929b122bdbc5b0ae
Patrick Scheibe <patrick@halirutan.de>
2017-02-26 04:29:16 +0100

    Pushed new version

--- SETools/Version.m
+++ SETools/Version.m
@@ -1,1 +1,1 @@
-{"Version" -> 128, "Changes" -> {"Replaced the shortened Decode.m URL with a real URL pointing to the GitHub repository. This fixes the issue with StackExchange no longer accepting shortened URLs."}}
+{"Version" -> 129, "Changes" -> {"Fixed copy to clipboard for versions >= 10"}}
